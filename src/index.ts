635413202d7e0aa85f5e9ad7f8f5c823c0f7ba02
hgenc <hgenc@hubspot.com>
2025-01-17 22:06:09 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 export default {
 	async fetch(request) {
 		// someHost is set up to return JSON responses
-		const someHost = 'https://jsonplaceholder.typicode.com';
+		const someHost = 'https://jsonplaceholder.typicode.coms';
 		const url1 = someHost + '/todos/1';
 		const url2 = someHost + '/todos/2';
 
-		const responses = await Promise.all([fetch(url1), fetch(url2)]);
-		const results = await Promise.all(responses.map((r) => r.json()));
-
-		const options = {
-			headers: { 'content-type': 'application/json;charset=UTF-8' },
-		};
-		return new Response(JSON.stringify(results), options);
+		let results;
+		try {
+			const resp1 = await fetch(url1);
+			const resp2 = await fetch(url2);
+			const json1 = await resp1.json();
+			const json2 = await resp2.json();
+			results = JSON.stringify([json1, json2], null, 2);
+		} catch (e: any) {
+			results = `${e.message}\n${e.stack}`;
+		}
+		return new Response(results);
 	},
 } satisfies ExportedHandler;
